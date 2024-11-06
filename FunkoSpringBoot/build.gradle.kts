a514557587fad449a18796bfbbaa6dd1d8b1a4b2
Alba García Orduña <albag448@gmail.com>
2024-11-06 15:43:19 +0100

    Implementando jacoco

--- FunkoSpringBoot/build.gradle.kts
+++ FunkoSpringBoot/build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("jacoco")
 }
 
-group = "com.example"
+group = "org.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -25,19 +26,30 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-	implementation("com.h2database:h2")
-
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy("jacocoTestReport")
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(false)
+		csv.required.set(false)
+		html.required.set(true)
+	}
+}
+
+tasks.check {
+	dependsOn(tasks.jacocoTestReport)
 }
